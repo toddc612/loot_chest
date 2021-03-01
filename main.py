d172fba62606594efa0e8a6998970f0e09ca559d
Todd Cermak <tcermak@goempyrean.com>
2021-03-01 15:46:08 -0600

    Changes.

--- main.py
+++ main.py
@@ -3,47 +3,33 @@ import random
 
 def get_monster():
     monsters = pd.read_csv("data/monstats.txt", delimiter="\t")
+    monsters.columns = ["class", "type", "level", "treasureclass"]
     rows = monsters.shape[0]
     monster = monsters.iloc[random.randint(0, (rows-1))]
     return monster
 
 def get_base_item(treasure_class):
-    # item = get_treasure(treasure_class)
-    # print(item)
-
-    print ("STARTING TREASURE CLASS: ", treasure_class)
-
     is_item = False
 
     while not is_item:
-        print("-->> LOOKING UP: ", treasure_class)
         item = get_treasure(treasure_class)
 
-        print("ITEM:", item)
-        print("PREFIX:", item[:2])
-        print("CHECK:", item[:2] == 'tc')
-
-        # If item is prefixed with "tc:", it means references another treasure class
-        # so we need to do another lookup with what this value is.
-
-        # Parse "tc:something" to get the class value.
-
-        # If not prefixed with "tc:", then return this value as the base item.
-
         if item[:2] == 'tc':
-            print("MATCHED!\n\n\n")
             treasure_class = item
             continue
         else:
-            print("GOT AN OBJECT:", item)
             is_item = True
+            return item
 
+def get_item_stats(base_item):
+    armor_stats = pd.read_csv("data/armor.txt", delimiter="\t")
 
 
 
 def get_treasure(treasure_class):
     treasures = pd.read_csv("data/TreasureClassEx.txt", delimiter="\t")
-    treasure = treasures.loc[treasures['Treasure Class'] == treasure_class]
+    treasures.columns = ["treasureclass", "item1", "item2", "item3"]
+    treasure = treasures.loc[treasures['treasureclass'] == treasure_class]
     item = treasure.iloc[:, (random.randint(1, 3))]
     return(item.values[0])
 
@@ -51,9 +37,13 @@ def get_treasure(treasure_class):
 if __name__ == '__main__':
     print('\n--->> Loot Chest v1.0 <<---\n')
     monster = get_monster()
-    print("MONSTER:", monster["Class"])
-    print("TREASURE CLASS:", monster["TreasureClass"])
-    base_item = get_base_item(monster["TreasureClass"])
+    print("You have slain a ", monster["class"])
+    print("TREASURE CLASS:", monster["treasureclass"])
+    base_item = get_base_item(monster["treasureclass"])
+    print("BASE ITEM: ", base_item)
+    item_stats = get_item_stats(base_item)
+
+
 
 
 
