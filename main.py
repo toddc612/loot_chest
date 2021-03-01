7943db4dc3a8596e81d316fe870a94cfe43eb9f1
Todd Cermak <tcermak@goempyrean.com>
2021-03-01 07:48:05 -0600

    Changes.

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import random
 
-def print_hi(name):
+def intro(name):
     # Use a breakpoint in the code line below to debug your script.
     print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
+def get_monster():
+    monsters = pd.read_csv("data/monstats.txt", delimiter="\t")
+    monsters.columns = ["class", "type", "level", "treasureclass"]
+    print(monsters.iloc[1])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    intro('PyCharm')
+    get_monster()
 
 
-monsters = pd.read_csv("data/monstats.txt", delimiter="\t")
-print(monsters.iloc[1])
+# monsters = pd.read_csv("data/monstats.txt", delimiter="\t")
+# print(monsters.iloc[1])
