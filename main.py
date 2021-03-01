0e4882b416b6ecdf8905f889ea8704609f338b2b
Todd Cermak <tcermak@goempyrean.com>
2021-03-01 10:44:23 -0600

    Changes.

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ def get_monster():
     monsters.columns = ["class", "type", "level", "treasureclass"]
     rows = monsters.shape[0]
     monster = monsters.iloc[random.randint(0, (rows-1))]
-    print(monster)
+    return monster
+
+def get_base_item(treasure_class):
+    treasures = pd.read_csv("data/TreasureClassEx.txt", delimiter="\t")
+    treasures.columns = ["treasureclass", "item1", "item2", "item3"]
+    treasure = treasures.loc[treasures["treasureclass"] == treasure_class]
+    print(treasure)
 
 
 
 if __name__ == '__main__':
     print('\n--->> Loot Chest v1.0 <<---\n')
-    get_monster()
+    monster = get_monster()
+    print("MONSTER:", monster["class"])
+    print("TREASURE CLASS:", monster["treasureclass"])
+    base_item = get_base_item(monster["treasureclass"])
+
 
 
